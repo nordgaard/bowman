aa7ba9a236eb780c92d759190abbc49e25bfa4a2
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-04-25 11:00:40 -0700

    CRUD in

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,24 @@ class ProductsController < ApplicationController
 		redirect_to product
 	end
 
+	def edit
+	    @product = Product.find_by(id: params[:id])
+	    render :edit
+	end
+
+	def update
+        product = Product.find_by(id: params[:id])
+        updated_attributes = ({name: params[:name], description: params[:description], price: params[:price]})
+        product.update_attributes(updated_attributes)
+        redirect_to product
+    end
+
+    def destroy
+        product = Product.find_by(id: params[:id])
+        product.destroy
+        redirect_to danimals_path
+    end
+
 
 
 end
