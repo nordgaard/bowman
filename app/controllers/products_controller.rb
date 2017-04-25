cc9f394495c1c5d2fc91eb6d3b8a258c2e8f7ce7
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-04-25 10:45:13 -0700

    adding views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,19 @@
 class ProductsController < ApplicationController
 
-def index
+	def show
+		@product = Product.find_by(id: params[:id])
+		render :show
+	end
 
-	@products = Product.all
-	render :index
-	
-end
+	def index
+		@products = Product.all
+		render :index
+	end
+
+	def create
+		product = Product.create(name: params[:name], description: params[:description])
+		redirect_to product
+	end
 
 
 
